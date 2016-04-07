fadf22c3dfee03f4bcd37bfd75e744b66786ff05
shulga-igor <igorshulga@yhoo.com>
2016-04-07 12:05:47 +0300

    push

--- src/main/java/HomeSeleniumTest.java
+++ src/main/java/HomeSeleniumTest.java
@@ -6,13 +6,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.FindBy;
 
-//test
 
 public class HomeSeleniumTest extends TestCase {
     private WebDriver driver = new FirefoxDriver();
 
     private By wikiSearchInput = By.id("searchInput");
+    private By firstHeadingOnPage = By.id("firstHeading");
+
+
+
 
     @BeforeClass
     public void setUp() throws Exception{
@@ -20,8 +24,29 @@ public class HomeSeleniumTest extends TestCase {
     }
     @Test
     public void testWiki () throws Exception{
+
+        // looking ror Selenium IDE
+
         WebElement searchInput = driver.findElement(wikiSearchInput);
         searchInput.sendKeys("Selenium IDE \n");
+        WebElement seleniumLabel = driver.findElement(firstHeadingOnPage);
+        String label = seleniumLabel.getText();
+        System.out.println("Wikipedia page " + label + " opened");
+        assertTrue("Wrong page", label.contains("Selenium"));
+
+
+        // looking for Chuck Norris! o_O
+        driver.findElement(wikiSearchInput).sendKeys("Chuck Norris \n");
+
+        Thread.sleep(5000);
+
+         //driver.findElement(By.id("Selenium_IDE"));
+
+
+
+
+        //Selenium (software)
+
 
     }
 
